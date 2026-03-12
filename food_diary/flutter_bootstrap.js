04471c8aaf110188c3129ca5afd975f72a93d463
Jake MacDonald <jakemac53@gmail.com>
2026-03-12 08:18:27 -0700

    "deploy food diary app"

--- food_diary/flutter_bootstrap.js
+++ food_diary/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"98977409b2ac8209be9e918abb0bbb9c37a23d0f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"873d02183cb670c541efaedfdb1a60066b894cb6","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2835307218" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2052100513" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
