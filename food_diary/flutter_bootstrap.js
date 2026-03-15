cedd97cc2f6d1997076350bd6f127d4a3c689d31
Jake MacDonald <jakemac53@gmail.com>
2026-03-15 16:11:38 -0700

    "deploy food diary app"

--- food_diary/flutter_bootstrap.js
+++ food_diary/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"873d02183cb670c541efaedfdb1a60066b894c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2747175191" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2065773762" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
