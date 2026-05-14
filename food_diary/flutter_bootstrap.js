0808bb124b5f6f74fb842b35a8fe9ec327467775
Jake MacDonald <jakemac53@gmail.com>
2026-05-14 10:37:22 -0700

    "deploy nori app"

--- food_diary/flutter_bootstrap.js
+++ food_diary/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3d69471c0bf9604bb68c08ded5eed019bf3114
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3973683499" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3389087959" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
